a7a340f00e20d0a6699183a55bff5cf0a263be61
Devskiller <owl@devskiller.com>
2021-07-29 17:03:21 +0000

    File modification

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,1 +1,24 @@
-// You can create your Post model here.
+import moongose from "mongoose";
+
+const PostSchema = new moongose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = moongose.model("Post", PostSchema);
+
+export default Post;
